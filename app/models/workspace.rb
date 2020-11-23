68e7d04c862893e6abd956ddc2a0c9032732c34a
Eric Blandet <eric.blandet@gmail.com>
2020-11-23 17:16:50 +0100

    added reference to bookings + address in Show view

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -3,4 +3,6 @@ class Workspace < ApplicationRecord
   validates :name, :description, :address, :capacity, :daily_rate, presence: true
   validates :daily_rate, numericality: { greater_than: 0 }
   validates :capacity, numericality: { only_integer: true }
+
+  has_many :bookings
 end
