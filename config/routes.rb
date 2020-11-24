4f88ad2bd0d2e173e82025869f2c4084e77e48ae
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-11-24 16:58:56 +0100

    Changed the booking routes back

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :workspaces, only: [:index, :show, :new, :create] do
-    resources :bookings
-  end
+  resources :workspaces, only: [:index, :show, :new, :create]
+  resources :bookings
   authenticated :user do
     root 'bookings#index', as: 'authenticated_root'
   end
