7809adfca3d3dc5d2172d6fcc6ef7aaad344f397
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-11-24 16:39:05 +0100

    Route Updated for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :workspaces, only: [:index, :show, :new, :create] do
     resources :bookings
   end
-      authenticated :user do
-        root 'bookings#index', as: 'authenticated_root'
-    end
+  authenticated :user do
+    root 'bookings#index', as: 'authenticated_root'
+  end
+  root to: 'pages#home'
 end
