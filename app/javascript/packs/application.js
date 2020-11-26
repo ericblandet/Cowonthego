c285835b3195099f5129c83151ae809cf82c35b6
Eric Blandet <eric.blandet@gmail.com>
2020-11-26 19:20:34 +0100

    computation works but it is ugly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,9 +30,35 @@ import { initMapbox } from '../plugins/init_mapbox';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   loadDynamicBannerText();
   initMapbox();
+});
+
+const dailyRate = document.getElementById("daily_rate");
+const price = document.getElementById("computed_price");
+const startDateInput = document.getElementById("booking_start_date");
+const endDateInput = document.getElementById("booking_end_date");
+const numberOfPersonsInput = document.getElementById("booking_number_of_persons");
+const inputs = document.querySelectorAll(".form-control");
+
+const computedPrice = () => {
+  const dailyRateInt = parseInt(dailyRate.innerText.match(/\d+/)[0], 10);
+  const numberOfPersonsInt = parseInt(numberOfPersonsInput.value, 10);
+  let numberOfDays = (Date.parse(endDateInput.value) - Date.parse(startDateInput.value)) / (1000 * 60 * 60 * 24) + 1;
+  return numberOfDays * dailyRateInt * numberOfPersonsInt;
+}
+
+inputs.forEach((element) => {
+  element.addEventListener('change', (event) => {
+    let totalPrice = computedPrice();
+    if (isNaN(totalPrice)) {
+      price.innerText = "CHF";
+    } else {
+      price.innerText = `${computedPrice()} CHF`;
+    }
+  });
 });
