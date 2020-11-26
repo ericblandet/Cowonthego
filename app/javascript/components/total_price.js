e728c125c1c43db679d37264b72b95bde5c05caa
Eric Blandet <eric.blandet@gmail.com>
2020-11-26 19:34:48 +0100

    computation works

--- app/javascript/components/total_price.js
+++ app/javascript/components/total_price.js
@@ -5,20 +5,26 @@ const endDateInput = document.getElementById("booking_end_date");
 const numberOfPersonsInput = document.getElementById("booking_number_of_persons");
 const inputs = document.querySelectorAll(".form-control")
 
-const computedPrice = () => {
-  const dailyRateInt = parseInt(dailyRate.innerText.match(/\d+/)[0], 10);
-  const numberOfPersonsInt = parseInt(numberOfPersonsInput.value, 10);
-  let numberOfDays = (Date.parse(endDateInput.value) - Date.parse(startDateInput.value)) / (1000 * 60 * 60 * 24) + 1;
-  return numberOfDays * dailyRateInt * numberOfPersonsInt
+
+const computePrice = () => {
+  const computedPrice = () => {
+    const dailyRateInt = parseInt(dailyRate.innerText.match(/\d+/)[0], 10);
+    const numberOfPersonsInt = parseInt(numberOfPersonsInput.value, 10);
+    let numberOfDays = (Date.parse(endDateInput.value) - Date.parse(startDateInput.value)) / (1000 * 60 * 60 * 24) + 1;
+    return numberOfDays * dailyRateInt * numberOfPersonsInt
+  }
+
+    inputs.forEach((element) => {
+      element.addEventListener('change', (event) => {
+        let totalPrice = computedPrice();
+        if (isNaN(totalPrice)) {
+          price.innerText = "";
+        } else {
+          price.innerText = `${computedPrice()} CHF`;
+        }
+      });
+    });
+
 }
 
-inputs.forEach((element) => {
-  element.addEventListener('change', (event) => {
-    let totalPrice = computedPrice();
-    if (isNaN(totalPrice)) {
-      price.innerText = "";
-    } else {
-      price.innerText = `${computedPrice()} CHF`;
-    }
-  });
-});
+export { computePrice };
