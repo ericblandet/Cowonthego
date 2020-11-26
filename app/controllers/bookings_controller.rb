0dbb9a374b53ff40b8e23769008576839a5da45d
Eric Blandet <eric.blandet@gmail.com>
2020-11-26 13:37:55 +0100

    add comments

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -57,11 +57,23 @@ class BookingsController < ApplicationController
   end
 
   def is_there_still_place?
+    # Create an array of "dates" for each day booked
     date_array = (params[:booking][:start_date]..params[:booking][:end_date]).to_a
+    # In this Array, we need to verify for each element = day, that the capacity is not exceeded
     date_array.all? do |date|
-      bookings = Booking.where("bookings.workspace_id = :workspace_id AND :date >= bookings.start_date AND :date <= bookings.end_date", workspace_id: params[:workspace_id], date: date)
+      # Extract all the bookings corresponding to this day, and to to the workspace
+      bookings = Booking.where(
+                                "bookings.workspace_id = :workspace_id
+                                AND :date >= bookings.start_date
+                                AND :date <= bookings.end_date",
+                                workspace_id: params[:workspace_id],
+                                date: date
+                              )
+      # Sum all the persons of all the bookings of this day
       sum_bookings = bookings.sum(:number_of_persons)
+      # Add the number_of_persons the user wants to book for
       new_sum_bookings = sum_bookings + params[:booking][:number_of_persons].to_i
+      # Compares the future number of persons (if the booking is accepted) to the capacity of workspace
       new_sum_bookings <= Workspace.find(params[:workspace_id]).capacity
     end
   end
